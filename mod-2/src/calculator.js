6acae0a64db4469d79c53223a8b9380abc285951
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-05-09 18:59:37 +0200

    Advanced TypeScript

--- mod-2/src/calculator.js
+++ mod-2/src/calculator.js
@@ -1,18 +1,17 @@
+"use strict";
 class Logger {
     print(text) {
         console.log(text);
     }
 }
 class Calculator {
-    // private logger: Logger;
-    // constructor(logger: Logger) {
-    //   this.logger = logger;
-    // }
-    constructor(logger) {
+    constructor(logger = { print() { } }) {
         this.logger = logger;
     }
     add(x, y) {
-        this.logger.print('Aan het optellen');
+        if (this.logger) {
+            this.logger.print('Aan het optellen');
+        }
         return x + y;
     }
     subtract(x, y) {
@@ -32,16 +31,80 @@ const subtract = document.getElementById('subtract');
 const multiply = document.getElementById('multiply');
 const divide = document.getElementById('divide');
 const result = document.getElementById('result');
-const calculator = new Calculator(new Logger());
-add.addEventListener('click', () => {
-    result.textContent = calculator.add(+x.value, +y.value).toString();
-});
-subtract.addEventListener('click', () => {
-    result.textContent = calculator.subtract(+x.value, +y.value).toString();
-});
-multiply.addEventListener('click', () => {
-    result.textContent = calculator.multiply(+x.value, +y.value).toString();
-});
-divide.addEventListener('click', () => {
-    result.textContent = calculator.divide(+x.value, +y.value).toString();
-});
+// const calculator = new Calculator(new Logger());
+// const calculator = new Calculator(null);
+const calculator = new Calculator();
+if (result) {
+    if (add) {
+        add.addEventListener('click', () => {
+            result.textContent = calculator.add(+x.value, +y.value).toString();
+        });
+    }
+    subtract.addEventListener('click', () => {
+        result.textContent = calculator.subtract(+x.value, +y.value).toString();
+    });
+    multiply.addEventListener('click', () => {
+        result.textContent = calculator.multiply(+x.value, +y.value).toString();
+    });
+    divide.addEventListener('click', () => {
+        result.textContent = calculator.divide(+x.value, +y.value).toString();
+    });
+}
+function doeMeer(o) {
+    console.log(o.firstName);
+}
+function doIets(o) {
+    if (o.firstName) {
+        doeMeer(o);
+    }
+}
+doIets(1);
+doIets({ firstName: 'Maurice' });
+// doeMeer(1)
+function geefNiets() {
+    throw new Error();
+}
+function useAnimal(beestje) {
+    if (beestje.type === 'hond') {
+        beestje.blaf();
+    }
+    else if (beestje.type === 'kat') {
+        beestje.miauw();
+    }
+    else {
+        beestje.blub();
+    }
+}
+// const katEnHond: KatEnHond = {
+// type: 'kat'&& 'hond',
+// //'kat' && 'hond',
+// blaf(){},
+// miauw(){}
+// };
+// class Demo implements KatEnHond {
+// }
+function eenNaam(param) { }
+eenNaam(0);
+eenNaam(1);
+// eenNaam(3);
+eenNaam(true);
+eenNaam(false);
+eenNaam('true');
+async function getAsyncStuff() {
+    try {
+        const rsp = await fetch('http://api.icndb.com/jokes/random/10/?limitTo=[nerdy]&escape=javascript');
+        if (rsp.ok) {
+            const data = await rsp.json();
+            console.log(data);
+            return data;
+        }
+        else {
+            throw new Error(rsp.statusText);
+        }
+    }
+    catch {
+        console.log('oops');
+    }
+    return null;
+}
+getAsyncStuff();
--- mod-2/src/calculator.ts
+++ mod-2/src/calculator.ts
@@ -9,16 +9,13 @@ class Logger implements LoggerInterface {
 }
 
 class Calculator {
-  // private logger: Logger;
-
-  // constructor(logger: Logger) {
-  //   this.logger = logger;
-  // }
-
-  constructor(private readonly logger: LoggerInterface) {}
+  constructor(private readonly logger: LoggerInterface | null = {print(){}}) {}
 
   add(x: number, y: number) {
-    this.logger.print('Aan het optellen');
+    if (this.logger) {
+      this.logger.print('Aan het optellen');
+    }
+
 
     return x + y;
   }
@@ -43,20 +40,125 @@ const multiply = document.getElementById('multiply');
 const divide = document.getElementById('divide');
 const result = document.getElementById('result');
 
-const calculator = new Calculator(new Logger());
+// const calculator = new Calculator(new Logger());
+// const calculator = new Calculator(null);
+const calculator = new Calculator();
 
-add.addEventListener('click', () => {
-  result.textContent = calculator.add(+x.value, +y.value).toString();
-});
+if (result) {
+  if (add) {
+    add.addEventListener('click', () => {
+      result.textContent = calculator.add(+x.value, +y.value).toString();
+    });
+  }
 
-subtract.addEventListener('click', () => {
-  result.textContent = calculator.subtract(+x.value, +y.value).toString();
-});
+  subtract!.addEventListener('click', () => {
+    result.textContent = calculator.subtract(+x.value, +y.value).toString();
+  });
 
-multiply.addEventListener('click', () => {
-  result.textContent = calculator.multiply(+x.value, +y.value).toString();
-});
+  multiply!.addEventListener('click', () => {
+    result.textContent = calculator.multiply(+x.value, +y.value).toString();
+  });
 
-divide.addEventListener('click', () => {
-  result.textContent = calculator.divide(+x.value, +y.value).toString();
-});
+  divide!.addEventListener('click', () => {
+    result.textContent = calculator.divide(+x.value, +y.value).toString();
+  });
+}
+
+type Person = {
+  firstName: string;
+};
+function doeMeer(o: Person) {
+  console.log(o.firstName);
+}
+
+function doIets(o: unknown) {
+  if ((o as Person).firstName) {
+    doeMeer(o as Person);
+  }
+}
+
+doIets(1);
+doIets({ firstName: 'Maurice' });
+
+// doeMeer(1)
+
+function geefNiets(): never {
+  throw new Error();
+}
+
+// const x1 = geefNiets();
+
+type Hond = {
+  type: 'hond';
+  blaf(): any;
+};
+
+type Kat = {
+  type: 'kat';
+  miauw(): void;
+};
+
+type Vis = {
+  type: 'vis';
+  blub(): string;
+};
+
+type KatOfHondOfVis = Kat | Hond | Vis;
+type KatEnHond = Kat & Hond;
+
+function useAnimal(beestje: KatOfHondOfVis) {
+  if (beestje.type === 'hond') {
+    beestje.blaf();
+  } else if (beestje.type === 'kat') {
+    beestje.miauw();
+  } else {
+    beestje.blub();
+  }
+}
+
+// const katEnHond: KatEnHond = {
+// type: 'kat'&& 'hond',
+
+// //'kat' && 'hond',
+
+// blaf(){},
+// miauw(){}
+
+// };
+
+// class Demo implements KatEnHond {
+
+// }
+
+function eenNaam(param: 0 | 1 | boolean | 'true') {}
+
+eenNaam(0);
+eenNaam(1);
+// eenNaam(3);
+eenNaam(true);
+eenNaam(false);
+eenNaam('true');
+
+type Iets = Partial<KatOfHondOfVis>;
+
+async function getAsyncStuff() {
+  try {
+    const rsp = await fetch(
+      'http://api.icndb.com/jokes/random/10/?limitTo=[nerdy]&escape=javascript'
+    );
+
+    if (rsp.ok) {
+      const data = await rsp.json();
+      console.log(data);
+
+      return data;
+    } else {
+      throw new Error(rsp.statusText);
+    }
+  } catch {
+    console.log('oops');
+  }
+  return null;
+}
+
+getAsyncStuff();
